8306660218871575d2c57cb1c95d4df0dbfdeed4
Meria Thomas and Skye Nguyen <skye@>
2018-08-22 11:08:43 -0700

    remove spaces

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,6 @@ function Dice() {
 Dice.prototype.rollDice= function() {
   this.diceValue = Math.floor(Math.random()* 6) +1;
 }
-
 Player.prototype.getTurnScore = function () {
   this.turnScore = (newDice.diceValue === 1) ? (this.turnScore = 0) : (this.turnScore += newDice.diceValue);
 }
@@ -17,11 +16,9 @@ Player.prototype.getTotalScore = function () {
   this.totalScore += this.turnScore;
   this.turnScore = 0;
 }
-
 var playerOne = new Player("1");
 var playerTwo = new Player("2");
 var newDice = new Dice();
-
 $(document).ready(function() {
   $("#rollDiceOne").click(function() {
     newDice.rollDice();
@@ -29,7 +26,6 @@ $(document).ready(function() {
       $("#rollDiceOne").hide();
       $("#rollDiceTwo").show();
     }
-
     playerOne.getTurnScore();
     $("#diceValues").text(newDice.diceValue);
     $("#playerOneTurn").text(playerOne.turnScore);
@@ -45,15 +41,12 @@ $(document).ready(function() {
     $("#playerTwoTurn").text(playerTwo.turnScore);
   });
   $("#hold").click(function() {
-
-    // console.log(playerOne.totalScore);
     if (playerOne.turnScore === 0) {
       playerTwo.getTotalScore();
       if (playerTwo.totalScore >= 20) {alert ("Player Two Won!" )}
       $("#rollDiceOne").show();
       $("#rollDiceTwo").hide();
       $("#playerTwoTotal").text(playerTwo.totalScore);
-
     }
     else {
       playerOne.getTotalScore();
