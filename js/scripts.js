470e0b33937a83d81086fe8b15f388719a5bcb5e
Meria Thomas and Skye Nguyen <skye@>
2018-08-22 08:58:27 -0700

    add back end logic to generate a dice value

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,22 @@ function Player(id) {
   this.id = id;
   this.totalScore = 0;
   this.currentScore = 0;
-  this.turnScore =0;
+  this.turnScore = 0;
 }
+function Dice() {
+  this.diceValue = 0;
+}
+Dice.prototype.rollDice= function() {
+  this.diceValue = Math.floor(Math.random()* 6) +1;
+}
+
+var playerOne = new Player("1");
+var playerTwo = new Player("2");
+var newDice = new Dice();
+
+$(document).ready(function() {
+  $("#rollDice").click(function() {
+    newDice.rollDice();
+    console.log(newDice.diceValue);
+  });
+});
